c9db67a9215e3e60c220e900bc99af63ec60abcd
David Adams <onethinline@gmail.com>
2020-07-09 08:31:38 -0700

    Add dynamic color options based on theme chosen

--- js/script.js
+++ js/script.js
@@ -2,34 +2,62 @@ const nameInput = document.getElementById('name');
 const jobOtherInput = document.getElementById('other-title');
 const colorOptionsDiv = document.getElementById('colors-js-puns');
 const designSelect = document.getElementById('design');
+const colorSelect = document.getElementById('color');
+const colorOptions = document.querySelectorAll('#color option');
+// const colorOptions = document.querySelectorAll('#color option');
 
-// Hide not-yet-relevant elements
+// Hide color options and set select to "Please select" option
+function hideColorOptions() {
+  colorSelect.selectedIndex = 0;
+  for (let i = 0; i < colorOptions.length; i++) {
+    colorOptions[i].hidden = true;
+  };
+};
+
+// Initial set-up steps
 jobOtherInput.style.display = 'none';
-colorOptionsDiv.style.display = 'none';
+const pleaseOption = document.createElement('option');
+pleaseOption.textContent = "Please select a T-shirt theme";
+colorSelect.add(pleaseOption, 0);
+hideColorOptions();
+// colorOptionsDiv.style.display = 'none';
 
-// Display appropriate subset of color options based on chosen theme
-// Receive selected theme.
-// Show colorOptionsDiv if theme is one of the two choices (hide if not)
-// Select the color options into an array (or two arrays)
-// Based on theme, hide/disable non-matching set of colors and enable matching set
-
-function updateColors(theme) {
+function updateColors(themeText) {
+  // Extract the category portion of the theme choice text
   const colorOptions = document.querySelectorAll('#color option');
-  console.log(colorOptions);
+  const matchText = themeText.replace('Theme - ', '');
+  const matchRegEx = new RegExp(matchText);
+  let newIndexIsSet = false;
 
-  if (theme === 'Select Theme') {
-    colorOptionsDiv.style.display = 'none';
+  // Reset things if no theme is chosen
+  if (themeText === 'Select Theme') {
+    // colorOptionsDiv.hidden = true;
+    colorSelect.selectedIndex = 0;
+    hideColorOptions();
   } else {
-    colorOptionsDiv.style.display = 'inherit';
-  }
+    // colorOptionsDiv.hidden = false;
+  };
 
-  
-}
+  // Iterate through colorOptions and hide/show based on matching matchText
+  for (let i = 0; i < colorOptions.length; i++) {
+    const optionText = colorOptions[i].text;
+    if (matchRegEx.test(optionText)) {
+      colorOptions[i].hidden = false;
+      if (!newIndexIsSet) {
+        colorSelect.selectedIndex = i; // Set our new default selection
+        newIndexIsSet = true;
+      }
+    } else {
+      colorOptions[i].hidden = true;
+    };
+  };
+
+};
 
 // Add event listeners
 designSelect.addEventListener('change', (e) => {
-  console.log(e.target.value);
-  updateColors(e.target.value)
+  const selectedText = designSelect.options[designSelect.selectedIndex].text;
+  updateColors(selectedText);
 });
 
 // When page loads, set focus on the first field
